c3ad9a8753b798456b806c99464f7162340c6aec
jesse Lloyd <jesselloyd911@gmail.com>
2023-02-03 05:14:43 -0500

    refactor info

--- src/components/ShowCryptoDetails.js
+++ src/components/ShowCryptoDetails.js
@@ -1,10 +1,10 @@
 function ShowCryptoDetails({ data }) {
   return (
     <div>
-      <h1>About {data.data.id}</h1>
-      <h2>ranking {data.data.rank}</h2>
-      <h3>supply {data.data.supply}</h3>
-      <p>price {data.data.priceUsd}</p>
+      <h1>About: {data.data.id}</h1>
+      <h2>Global ranking: {data.data.rank}</h2>
+      <h3>Total Supply: {data.data.supply}</h3>
+      <p> Price: ${data.data.priceUsd}</p>
     </div>
   );
 }

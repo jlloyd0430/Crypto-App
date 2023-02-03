b399556bc50cdee4273ba158b49d8eca5eaa31ad
jesse Lloyd <jesselloyd911@gmail.com>
2023-02-03 05:20:54 -0500

    css change

--- src/CryptoAxios.js
+++ src/CryptoAxios.js
@@ -21,10 +21,11 @@ function CryptoAxios() {
       <h1>A Cryptocurrency and Exchange Search app!</h1>
       <form onSubmit={handleOnSubmit}>
         <input type="text" onChange={(e) => setCrypto(e.target.value)} />
-        <button type="submit">Submit</button>
+        <button type="submit">Search</button>
       </form>
-      <p>Cryptocurrency: {crypto}</p>
       <div className="data">
+        <p>Cryptocurrency: {crypto}</p>
+
         {/* Only shows is loading is true */}
         {loading && <Spinner />}
         {/* if loading is set to false, and the data exists, then it renders the ShowCryptoDetails component */}
